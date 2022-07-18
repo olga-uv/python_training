a969f8344d1d6c7443652f0a5ee7b30886541204
Olga Uvarova <leonteva.h@gmail.com>
2022-07-18 16:31:05 +0300

    session helper has been created for sessions management

--- fixture/application.py
+++ fixture/application.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.firefox.webdriver import WebDriver
 from selenium.webdriver.support.select import Select
+from fixture.session import SessionHelper
 
 
 class Application:
@@ -7,6 +8,7 @@ class Application:
     def __init__(self):
         self.wd = WebDriver()
         self.wd.implicitly_wait(30)
+        self.session = SessionHelper(self)
 
     def open_home_page(self):
         wd = self.wd
@@ -15,16 +17,6 @@ class Application:
     def open_add_new_contact_page(self):
         wd = self.wd
         wd.find_element_by_link_text("add new").click()
-
-    def login(self, username, password):
-        wd = self.wd
-        self.open_home_page()
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//input[@value='Login']").click()
 
     def open_groups_page(self):
         wd = self.wd
@@ -130,10 +122,6 @@ class Application:
     def return_to_home_page(self):
         wd = self.wd
         wd.find_element_by_link_text("home").click()
-
-    def logout(self):
-        wd = self.wd
-        wd.find_element_by_link_text("Logout").click()
 
     def destroy(self):
         self.wd.quit()
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -13,9 +13,9 @@ def app(request):
 
 
 def test_add_contact(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.create_contact(Contact(firstname="asd", middlename="asd", lastname="asd", nickname="asd", title="asd",
                                         company="asd", address="asd", home="asd", mobile="asd", work="asd", fax="asd",
                                         email="asd", email2="asd", email3="asd", homepage="asd", byear="1111",
                                         ayear="2222", address2="asdf", phone2="asdf", notes="asdf"))
-    app.logout()
+    app.session.logout()
