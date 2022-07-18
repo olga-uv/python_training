92cc2f95981cc7c806f93c7df207290c79d49dd7
Olga Uvarova <leonteva.h@gmail.com>
2022-07-18 16:41:09 +0300

    group helper has been created for working with groups

--- fixture/application.py
+++ fixture/application.py
@@ -1,5 +1,7 @@
 from selenium.webdriver.firefox.webdriver import WebDriver
 from selenium.webdriver.support.select import Select
+
+from fixture.group import GroupHelper
 from fixture.session import SessionHelper
 
 
@@ -9,6 +11,7 @@ class Application:
         self.wd = WebDriver()
         self.wd.implicitly_wait(30)
         self.session = SessionHelper(self)
+        self.group = GroupHelper(self)
 
     def open_home_page(self):
         wd = self.wd
@@ -18,28 +21,6 @@ class Application:
         wd = self.wd
         wd.find_element_by_link_text("add new").click()
 
-    def open_groups_page(self):
-        wd = self.wd
-        wd.find_element_by_link_text("groups").click()
-
-    def create_group(self, group):
-        wd = self.wd
-        self.open_groups_page()
-        # init group creation
-        wd.find_element_by_name("new").click()
-        # fill group form
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.header)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.footer)
-        # submit group creation
-        wd.find_element_by_name("submit").click()
-        self.return_to_groups_page()
 
     def create_contact(self, contact):
         wd = self.wd
@@ -114,10 +95,6 @@ class Application:
         wd.find_element_by_name("notes").send_keys(contact.notes)
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
         self.return_to_home_page()
-
-    def return_to_groups_page(self):
-        wd = self.wd
-        wd.find_element_by_link_text("group page").click()
 
     def return_to_home_page(self):
         wd = self.wd
