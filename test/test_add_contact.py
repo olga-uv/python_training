f41679fedd169eab78c74cf2bf1c1000fc3bf920
Olga Uvarova <leonteva.h@gmail.com>
2022-07-18 22:24:24 +0300

    test for deletion first group has been edded

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,15 +1,6 @@
 # -*- coding: utf-8 -*-
-import pytest
 
-from fixture.application import Application
 from model.contact import Contact
-
-
-@pytest.fixture()
-def app(request):
-    fixture = Application()
-    request.addfinalizer(fixture.destroy)
-    return fixture
 
 
 def test_add_contact(app):
@@ -19,3 +10,4 @@ def test_add_contact(app):
                                email="asd", email2="asd", email3="asd", homepage="asd", byear="1111",
                                ayear="2222", address2="asdf", phone2="asdf", notes="asdf"))
     app.session.logout()
+
