64551f72a1b8f83ccf48876de1be9e8a39fa64d0
Olga Uvarova <leonteva.h@gmail.com>
2022-07-22 23:59:29 +0300

    asserts for group tests added (for groups len before and after

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -4,8 +4,14 @@ from model.group import Group
 
 
 def test_add_group(app):
-    app.group.create(Group(name="asdfg", header="assd", footer="asdf"))
+    old_groups = app.group.get_group_list()
+    app.group.create(Group(name="Test name", header="Test header", footer="Test footer"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
 
 
 def test_add_empty_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group(name="", header="", footer=""))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
