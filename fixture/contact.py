a6ca3e0046dd6087fcf23429a8876235479a8734
Olga Uvarova <leonteva.h@gmail.com>
2022-07-22 15:09:51 +0300

    precondition for checking current url has been added

--- fixture/contact.py
+++ fixture/contact.py
@@ -7,7 +7,8 @@ class ContactHelper:
 
     def open_add_new_contact_page(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("add new").click()
+        if not (wd.current_url.endswith("/edit.php") and len(wd.find_elements_by_name("submit")) > 0):
+            wd.find_element_by_link_text("add new").click()
 
     def change_field_value(self, field_name, text):
         wd = self.app.wd
