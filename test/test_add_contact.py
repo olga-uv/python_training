608bbca39556f2cee118f3d0d2dca55dc5df41b8
Olga Uvarova <leonteva.h@gmail.com>
2022-07-24 01:08:30 +0300

    not working accesr for modify_contact test

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -5,11 +5,15 @@ from model.contact import Contact
 
 def test_add_contact(app):
     old_contacts = app.contact.get_contacts_list()
-    app.contact.create(Contact(firstname="name", middlename="middlename", lastname="lastname", nickname="nick",
+    contact = Contact(firstname="name", middlename="middlename", lastname="lastname", nickname="nick",
                                title="title", company="comp", address="adr", home_phone="phone", mobile_phone="phone",
                                work_phone="phone", fax="fax", email="email", email2="email", email3="email",
-                               homepage="site", address2="adr", phone2="phone", notes="notes"))
+                               homepage="site", address2="adr", phone2="phone", notes="notes")
+    app.contact.create(contact)
     new_contacts = app.contact.get_contacts_list()
     assert len(old_contacts) + 1 == len(new_contacts)
+    old_contacts.append(contact)
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
+
 
 
--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -5,7 +5,15 @@ def test_modify_first_contact_name(app):
     if app.contact.count() == 0:
         app.contact.create(Contact(firstname="First name"))
     old_contacts = app.contact.get_contacts_list()
-    app.contact.modify_first_contact(Contact(firstname="First Name"))
+    print(old_contacts)
+    contact = Contact(firstname="New Name")
+    contact.id = old_contacts[0].id
+    app.contact.modify_first_contact(contact)
     new_contacts = app.contact.get_contacts_list()
     assert len(old_contacts) == len(new_contacts)
+    #old_contacts[0] = contact
+    print(old_contacts)
+    print(new_contacts)
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
+
 
